6820182276aeb182cbe277099d994ec2cc9c24e5
Thi Nguyễn <nguyenthisc1@gmail.com>
2024-11-30 15:49:17 +0700

    format

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,15 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
+    trailingComma: 'es5',
+    semi: false,
+    tabWidth: 4,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    arrowParens: 'always',
+    embeddedLanguageFormatting: 'auto',
+    htmlWhitespaceSensitivity: 'css',
+    printWidth: 500,
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+    plugins: ['prettier-plugin-tailwindcss'],
+}
